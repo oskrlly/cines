e5a94f81d0ed9154c8c6315025766e25fcc08136
oskrlly <oskrlly@gmail.com>
2022-11-14 23:49:14 -0600

    general modules were added

---
+++ cines/movies.py
@@ -0,0 +1,11 @@
+
+m1 = dict(range(7)( id = '1',
+            tilte = 'Rocky',
+            url = 'https://cdn.shopify.com/s/files/1/0057/3728/3618/products/7f8002ac04902c75d2ec48892a372554_98269645-85ee-4135-845c-fa6ff551cd58_480x.progressive.jpg?v=1573615902',
+            clasification = 'PG',
+           
+            functions = dict(
+                id_function = '1',
+                date = '14/11/2022',
+            )
+))
