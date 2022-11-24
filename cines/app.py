0cb6b558e5ff206b33bbbb443df49a1bb2aea6f3
oskrlly <oskrlly@gmail.com>
2022-11-23 20:28:45 -0600

    insert data base

--- cines/app.py
+++ cines/app.py
@@ -1,12 +1,20 @@
-from flask import Flask , jsonify
+from flask import Flask , jsonify, render_template, request
+from flask_mysqldb import MySQLdb
 from users_registred import users #para importar los archivos creados dentro del proyecto que se usaran
 from login import login
 from movies import m1
+
 app = Flask(__name__) #ejecutamo flask como servidor 
+
+dbase = MySQLdb.connect(
+    host ="localhost",
+    user ="root",
+    passwd  ="",
+    db ="cines")
 
 @app.route('/', methods=['GET']) #indicamos la ruta de incio con el metodo get 
 def ping (): #definimos una funcion para dar la bienvenida 
-    return jsonify({"response": "Bienvenido"})
+    return render_template('index.html')
 
 @app.route('/users')
 def usercine():
@@ -16,9 +24,21 @@ def usercine():
 def login_ses():
         return jsonify({"login" : login})
 
-@app.route('/movies')
+@app.route('/movies', methods=['POST'])
 def moviesfun():
-    return jsonify({"movies": m1})
+    if request.method == 'POST':
+        titulo = request.form['titulo']
+        url = request.form['url']
+        clasificacion = request.form['clasificacion']
+        funciones = request.form['funciones']
+        id_funciones = request.form['id_funciones']
+        fecha_hora = request.form['fecha_hora']
+        cur = MySQLdb.Connection()
+        cur.cursor = ('INSTER INTO movies ( titulo, url, clasificacion, funciones, id_funciones, fecha_hora) VALUES (%s, %s, %s, %s, %s, %s, %s)', 
+        (titulo, url, clasificacion, funciones, id_funciones, fecha_hora))
+        MySQLdb.Connection.commit()
+        
+    return 'recibido'
 
 if __name__ == '__main__': # Sejecutar solo si este archivo se esta ejecutando como el archivo principal 
     
--- cines/movies.py
+++ cines/movies.py
@@ -1,5 +1,5 @@
 
-m1 = dict(range(7)( id = '1',
+m1 = dict( id = '1',
             tilte = 'Rocky',
             url = 'https://cdn.shopify.com/s/files/1/0057/3728/3618/products/7f8002ac04902c75d2ec48892a372554_98269645-85ee-4135-845c-fa6ff551cd58_480x.progressive.jpg?v=1573615902',
             clasification = 'PG',
@@ -8,4 +8,4 @@ m1 = dict(range(7)( id = '1',
                 id_function = '1',
                 date = '14/11/2022',
             )
-))
+)
